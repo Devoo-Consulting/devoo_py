aebb4600ec61bc58ebec313189e8d032b4f855c2
Klixtos <lhuisy@gmail.com>
2023-10-14 15:44:30 +0200

    standard output changes

--- solver.py
+++ solver.py
@@ -3,6 +3,7 @@
 #                        October 14, 2023
 #        We use Z3 that is a theorem prover from Microsoft Research.
 
+import argparse
 import sys
 import z3
 import time
@@ -11,16 +12,19 @@ z3.set_option(model=True)
 #z3.set_option(rational_to_decimal=True)
 z3.set_param("parallel.enable", False)
 log = False
-duration = 0.0
+timed = False
+started = 0.0
 
-def logging(message, elapsed):
-    if log:
-        global duration
-        print(message + " elapsed time " + str(elapsed - duration) + " milliseconds")
-        duration = time.time()
+def logging(message, elapsed = 0.0):
+    if log and timed and elapsed > 0.0:
+        print(message + (f" timed {elapsed} milliseconds elapsed time" if timed else ""))
+    elif log:
+        print(message)
 
 def solve(clauses, total):
-    logging("Start building the linear system", time.time())
+    if timed:
+        started = time.time()
+    logging("Start building the linear system")
     s = z3.Solver()
     x = [ z3.Real('x%s' % (i + 1)) for i in range(total) ]
     for i in range(total):
@@ -30,10 +34,22 @@ def solve(clauses, total):
         s.add(x[list[0]-1] + x[list[1]-1] > 2.0/3.0)
         s.add(x[list[0]-1] + x[list[2]-1] > 2.0/3.0)
         s.add(x[list[1]-1] + x[list[2]-1] > 2.0/3.0)
-    logging("End building the linear system", time.time())    
-    logging("Start solving the linear system", time.time())    
+    if timed:
+        logging("Done building the linear system", time.time() - started)
+    else:
+        logging("Done building the linear system")
+
+    if timed:
+        started = time.time()
+
+    logging("Start solving the linear system")    
     result = s.check()
-    logging("End solving the linear system", time.time())
+
+    if timed:
+        logging("Done solving the linear system", time.time() - started)
+    else:
+        logging("Done solving the linear system")
+        
     if result == z3.unsat:
         print("s UNSATISFIABLE")
     elif result == z3.unknown:
@@ -57,19 +73,36 @@ def parse_dimacs(asserts):
     return result   
                        
 if __name__ == "__main__":
-    duration = time.time()
+
+    parser = argparse.ArgumentParser(description='Solve an NP-complete problem from a DIMACS file.')
+    parser.add_argument('-i', '--inputFile', type=str, help='Input file path', required=True)
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
+    parser.add_argument('-t', '--timer', action='store_true', help='Enable timer output')
+    args = parser.parse_args()
+
+    log = args.verbose
+    timed = args.timer
+
+
+
+    
     #Read and parse a dimacs file
-    logging("Start reading and parsing dimacs file", time.time())
-    temp = sys.argv[1]
+    logging("Pre-processing started")
+    if timed:
+        started = time.time()
+
     reader = z3.Solver()
-    reader.from_file(temp)
-    logging("End reading and parsing dimacs file", time.time()) 
+    reader.from_file(args.inputFile)
     #Format from dimacs
-    logging("Start formating on dimacs", time.time())
     asserts = reader.dimacs().splitlines()
     reader.reset()
     total = int(asserts[0].split(' ')[2])
     clauses = parse_dimacs(asserts[1:])
-    logging("End formating on dimacs", time.time())
+
+    if timed:
+        duration = time.time() - started
+        logging(f"Pre-processing done in: {duration} milliseconds elapsed time")
+    else:
+        logging("Pre-processing done")
     # NP-complete Solver
     solve(clauses, total)
